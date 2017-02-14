0a302c74dc4b432aad329a77a8fc0f79f088b3c4
Adam Sandström <adsa95@gmail.com>
2017-02-14 14:19:00 +0100

    Update example

--- example.js
+++ example.js
@@ -1,24 +1,24 @@
 "use strict"
 
-require('dotenv').config();
-
 let Freerider = require('./index.js');
 
 let routes = [
 	new Freerider.Route('göteborg', '*'),
 ];
 
-let sender = new Freerider.SlackSender(process.env.SLACK_HOOK_URL);
-
-let doStatic = false;
-
-if(doStatic){
-	// static
-	Freerider.Helper.search(routes, sender);	
-}else{
-	// instantiated
-	let f = new Freerider.Helper();
-	f.getTrips(function(){
-		f.search(routes, sender);
-	})
+// a notifier can be anything with a send-method that gets a formatted string
+let notifier = {
+	send: function(message){console.log(message)}
 }
+
+// When called in a static context, data is fetched and searched directly
+Freerider.Helper.search(routes, notifier);	
+
+// When using an instance, fetched data can be saved and reused
+let helper = new Freerider.Helper();
+helper.getTrips(function(trips){
+	// available trips are now saved in the instance, multiple searches with different routes and notifiers can be made without fetching data again
+	helper.search(routes, notifier);
+	// if you need to clear trips
+	helper.clearTrips();
+})
